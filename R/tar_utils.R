affabaf8d8a975b6c3749306c33ce26033d37301
Zack  Arno <zackarno@gmail.com>
2023-06-23 15:25:33 +0300

    targets specific funcs for flagging and loading hydrobasins

---
+++ R/tar_utils.R
@@ -0,0 +1,58 @@
+
+load_clipped_hydrobasins <- function(gdb,lyr_names, mask){
+  basins <- c(lyr_names) %>%
+    map(\(lyr_name){
+      basin <- st_read(dsn = gdb, layer = lyr_name) %>%
+        mutate(
+          level = lyr_name
+        )
+      basin_valid<- st_make_valid(basin)
+      basin_valid[mask,]
+    }) %>% 
+    set_names(lyr_names)
+}
+
+# alert helper function
+# only generate alert when boundary crossed from below
+lim_alert <- function(x, lim) {
+  x >= lim & lag(x) < lim
+}
+
+classify_google_nowcast_data <- function(nowcast=google_nowcast, rp_df=gauge_google_wb$return_period ){
+  
+  # since each gauge has unique RPs we need to split the nowcast data by gauge id before classifyng
+  nowcast_split <- nowcast %>% 
+    mutate(
+      gauge_id = paste0("hybas_",hybas_station)
+    ) %>% 
+    split(.$gauge_id)
+  
+  rp_df_long <-  rp_df %>% 
+    pivot_longer(
+      -gauge_id
+    ) %>% 
+    mutate(
+      rp_yr = parse_number(name)
+    )  
+  
+  nowcast_classified <- nowcast_split %>% 
+    imap_dfr(
+      \(dft,gid){
+        rp_df_temp<- rp_df_long %>% 
+          filter(gauge_id == gid)
+        dft %>% 
+          mutate(
+            rp_2_flag = lim_alert(x= prediction,lim=rp_df_temp[rp_df_temp$rp_yr==2,]$value),
+            rp_5_flag = lim_alert(x= prediction,lim=rp_df_temp[rp_df_temp$rp_yr==5,]$value),
+            rp_20_flag = lim_alert(x= prediction,lim=rp_df_temp[rp_df_temp$rp_yr==20,]$value),
+            lt_rp2 = prediction < rp_df_temp[rp_df_temp$rp_yr==2,]$value,
+            gte_rp2 = prediction >= rp_df_temp[rp_df_temp$rp_yr==2,]$value,
+            gte_rp5 = prediction >= rp_df_temp[rp_df_temp$rp_yr==5,]$value,
+            gte_rp20 =  prediction >= rp_df_temp[rp_df_temp$rp_yr==20,]$value
+          )
+      }
+    )
+  return(nowcast_classified)
+  
+  
+}
